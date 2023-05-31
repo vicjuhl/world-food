408dbf486e63809fbdf15ae61c1df0625edd9e4e
wylich <frederik.wylich@gmail.com>
2023-05-31 15:19:14 +0200

    Created and imported Tables BMI & Waste

--- sql_scripts/data_importer.sql
+++ sql_scripts/data_importer.sql
@@ -13,32 +13,71 @@ CREATE TABLE Countries(
     population integer,
     count_name text,
     subcontinent text,
-    continent float                      /* Could perhaps be changed to a integer (as index for a chronological stage number) */
+    continent text
 );
 
 CREATE TABLE Affordability(
     country text,                  /* Should be dropped since we use iso as foreign key */
-    iso text, 
+    iso_alpha text, 
     year integer, 
     affordable float
 );
 
-CREATE TABLE BMI(
-    iso text, 
+/* CREATE TABLE BMI(
+    iso_alpha text, 
     sex text, 
     year integer, 
     mean_BMI_urban float,
     mean_BMI_rural float
+); */
+
+CREATE TABLE BMI(
+    Country_Region_World text,
+    ISO text,
+    Sex text,
+    Year text,
+    Mean_BMI_urban text,
+    Mean_BMI_urban_lower_95 text,
+    Mean_BMI_urban_upper_95 text,
+    Mean_BMI_rural text,
+    Mean_BMI_rural_lower_95 text,
+    Mean_BMI_rural_upper_95 text,
+    Mean_BMI_urban_rural_diff text,
+    Mean_BMI_urban_rural_diff_lower_95 text,
+    Mean_BMI_urban_rural_diff_upper_95 text
 );
 
-CREATE TABLE Waste(
-    iso text, 
+/* CREATE TABLE Waste(
+    iso integer,
+    iso_alpha text, 
     year integer, 
     commodity text,
     loss_pct float, 
-    stage text                      /* Could perhaps be changed to a integer (as index for a chronological stage number) */
-);
+    stage text          );              Could perhaps be changed to a integer (as index for a chronological stage number) */
 
+CREATE TABLE Waste(
+    m49_code text,
+    country text,
+    region text,
+    cpc_code text,
+    commodity text,
+    year text,
+    loss_percentage text,
+    loss_percentage_original text,
+    loss_quantity text,
+    activity text,
+    food_supply_stage text,
+    treatment text,
+    cause_of_loss text,
+    sample_size text,
+    method_data_collection text,
+    reference text,
+    url text,
+    notes text
+);
 
 \copy Affordability FROM 'raw_data\food-prices.csv' WITH CSV HEADER DELIMITER ',';
 
+\copy BMI FROM 'raw_data\NCD_RisC_Nature_2019_age_standardised_country.csv' WITH CSV HEADER DELIMITER ',';
+
+\copy Waste FROM 'raw_data\food-loss-waste-Data.csv' WITH CSV HEADER DELIMITER ',';
