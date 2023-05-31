9f0ee80b9f22373b651a48d3fdb208ef9f456c4c
vicjuhl <victorkjellerupjuhl@me.com>
2023-05-31 14:02:09 +0200

    gitignore

---
+++ SQL_scripts/create_and_load_tables.sql
@@ -0,0 +1,13 @@
+/*
+drop table unaffordability;
+
+create table unaffordability (
+	entity text,
+	code text,
+	year_ int,
+	h_unaff_sh decimal
+);
+
+--\copy unaffordability FROM '/Users/victor/Documents/Uddannelse/Repos/world-food/raw_data/food-prices.csv' DELIMITER ',' CSV HEADER;
+*/
+select * from unaffordability;
