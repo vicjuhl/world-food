eb798f2a963b1ae1b072c1d49355c67d0623ffc2
vicjuhl <victorkjellerupjuhl@me.com>
2023-06-01 14:44:34 +0200

    minimal dummy frontend

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import psycopg2
 
 app = Flask(__name__)
@@ -11,3 +11,6 @@ conn = psycopg2.connect(
     port="5432"
 )
 
+@app.route("/")
+def index():
+    return render_template("index.html")
