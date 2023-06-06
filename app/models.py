87922e83ba3634b8fb175aaa61755de7ff1c19ca
wylich <frederik.wylich@gmail.com>
2023-06-06 17:20:37 +0200

    plot legend and RISK colors

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import conn, plot_dir
 import matplotlib
 import matplotlib.pyplot as plt
-import os
+import pandas as pd
 matplotlib.use('Agg')
 
 def fetch_test(fname: str):
@@ -16,10 +16,27 @@ def fetch_test(fname: str):
     cur.close()
     return result
 
+def get_all_regions():
+    cur = conn.cursor()
+    sql = """
+        SELECT sub_region
+        FROM (
+            SELECT distinct sub_region, region
+            FROM Countries
+            ORDER BY region, sub_region) C
+    """
+    cur.execute(sql)
+    result = cur.fetchall()
+    cur.close()
+    result_list = [elm[0] for elm in result]
+    return result_list
+
 def fetch_region(subregions: list[str]):
     cur = conn.cursor()
     sql = """
-        SELECT
+        SELECT sub_region, unaf_avg, bmi_avg
+		FROM
+		(SELECT region,
             sub_region,
             avg(unaffordable) unaf_avg,
             avg((mean_urban + mean_rural)/2) bmi_avg
@@ -29,8 +46,9 @@ def fetch_region(subregions: list[str]):
         JOIN affordability
             USING (iso_alpha)
         WHERE sub_region IN %s
-        GROUP BY country_name, sub_region
-    """
+        GROUP BY region, country_name, sub_region
+		ORDER BY region, sub_region) A
+    """    
     cur.execute(sql, [tuple(subregions)])
     result = cur.fetchall()
     cur.close()
@@ -40,24 +58,45 @@ def get_plot(subregions: list[str]):
     """Make scatterplot and return its figure."""    
     result = fetch_region(subregions)
     subregion, affordability, bmi_or_waste = zip(*result)
-    plt.scatter(affordability, bmi_or_waste) # TODO: add colorscheme to sub-regions
+
+    df = pd.DataFrame(dict(subregion=subregion, affordability = affordability, bmi_or_waste = bmi_or_waste))
+
+    fig, ax = plt.subplots(figsize=(14, 8))
+
+    colors = {
+        "Antarctica": 'gray',
+        "Northern America": 'yellow',
+        "Latin America and the Caribbean": 'red',
+        "Northern Europe": 'lightblue',
+        "Western Europe": 'navy',
+        "Eastern Europe": 'blue',
+        "Southern Europe": 'royalblue',
+        "Northern Africa": 'darkorange',
+        "Sub-Saharan Africa": 'goldenrod',
+        "Western Asia": 'lime',
+        "Central Asia": 'green',
+        "Eastern Asia": 'forestgreen',
+        "Southern Asia": 'darkgreen',
+        "South-eastern Asia": 'springgreen',
+        "Australia and New Zealand": 'darkviolet',
+        "Micronesia": 'purple',
+        "Melanesia": 'magenta',
+        "Polynesia": 'plum'
+        }
+
+    ax.scatter(df['affordability'], df['bmi_or_waste'], c=df['subregion'].map(colors))
+    
+    legend_labels = list(colors.keys())
+    legend_handles = [plt.Line2D([0], [0], marker='o', color='w', label=label, markerfacecolor=color, markersize=10)
+                      for label, color in colors.items()]
+    ax.legend(handles=legend_handles, labels=legend_labels, loc='upper right') #TODO: improve legend to only display subregions chosen (without mismatching colors)
+
     plt.xlabel('Affordability')
     plt.ylabel('BMI or Waste')
+    plt.tight_layout()
     return plt
 
 def update_plot(subregions: list[str]) -> None:
     plot = get_plot(subregions)
     plot.savefig(plot_dir / 'plot.png')
     plot.close()
-
-def get_all_regions():
-    cur = conn.cursor()
-    sql = """
-        SELECT DISTINCT sub_region
-        FROM Countries
-    """
-    cur.execute(sql)
-    result = cur.fetchall()
-    cur.close()
-    result_list = [elm[0] for elm in result]
-    return result_list
