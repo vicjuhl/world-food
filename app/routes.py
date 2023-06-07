33c03c7ad06e1bd8f12a21c1963812e85211422d
vicjuhl <victorkjellerupjuhl@me.com>
2023-06-07 15:08:41 +0200

    Load Preset

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
 from flask import render_template, request, Blueprint
-from app.models import update_plot, get_all_regions
+from app.models import update_plot, get_all_regions, get_all_preset_names, get_preset
 from app import app
 from app.utils.exceptions import NoDataException
 
 Router = Blueprint('Router', __name__)
 
+# Initialize states
 subregions_state = {region: False for region in get_all_regions()}
+presets_names = get_all_preset_names()
 
 def update_subregions(checked_subs: list[str]):
     """Set and remember values of subregion checkboxes."""
@@ -14,7 +16,23 @@ def update_subregions(checked_subs: list[str]):
 
 @app.route("/")
 def index():
-    return render_template("index.html", subregions=subregions_state)
+    return render_template(
+        "index.html",
+        subregions=subregions_state,
+        preset_names=presets_names
+    )
+
+@app.route("/load", methods=["POST"])
+def on_load():
+    p_name = request.form.get("presets")
+    print(p_name)
+    chosen_preset = get_preset(p_name)
+    update_subregions(chosen_preset["sub_regions"])
+    return render_template(
+        "index.html",
+        subregions=subregions_state,
+        preset_names=presets_names
+    )
 
 @app.route('/submit', methods=["POST"])
 def on_submit():
@@ -23,7 +41,15 @@ def on_submit():
     if checked_subs != []:
         try:
             update_plot(checked_subs)
-            return render_template('submitted.html', subregions=subregions_state)
+            return render_template(
+                'submitted.html',
+                subregions=subregions_state,
+                preset_names=presets_names
+            )
         except NoDataException:
             pass
-    return render_template('no_countries.html', subregions=subregions_state)
+    return render_template(
+        'no_countries.html',
+        subregions=subregions_state,
+        preset_names=presets_names
+    )
