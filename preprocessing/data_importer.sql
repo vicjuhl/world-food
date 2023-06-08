bda5b00d917d8ff0e2de267c4efa8998ade88771
vicjuhl <victorkjellerupjuhl@me.com>
2023-06-08 10:44:41 +0200

    Two presets tables; schema constraints

--- preprocessing/data_importer.sql
+++ preprocessing/data_importer.sql
@@ -1,11 +1,8 @@
-DROP TABLE IF EXISTS Countries;
-
+DROP TABLE IF EXISTS Countries CASCADE;
 DROP TABLE IF EXISTS Affordability;
-
 DROP TABLE IF EXISTS BMI;
 
--- DROP TABLE IF EXISTS Waste;
-
+DROP TABLE IF EXISTS Presets CASCADE;
 DROP TABLE IF EXISTS SubRegionPresets;
 
 CREATE TABLE Countries(
@@ -14,13 +11,16 @@ CREATE TABLE Countries(
     sub_region text,
     sub_region_code char(3),
     region text,
-    region_code char(3)
+    region_code char(3),
+    PRIMARY KEY (iso_alpha)
 );
 
 CREATE TABLE Affordability(
-    iso_alpha text,
+    iso_alpha char(3),
     year_ integer,
-    unaffordable float
+    unaffordable float,
+    PRIMARY KEY (iso_alpha, year_),
+    FOREIGN KEY (iso_alpha) REFERENCES Countries
 );
 
 CREATE TABLE BMI(
@@ -28,14 +28,31 @@ CREATE TABLE BMI(
     sex varchar(5),
     year_ integer,
     mean_urban float,
-    mean_rural float
+    mean_rural float,
+    PRIMARY KEY (iso_alpha, sex, year_),
+    FOREIGN KEY (iso_alpha) REFERENCES Countries
 );
+
+CREATE TABLE Presets(
+    pName text,
+    rural_urban text NOT NULL,
+    PRIMARY KEY (pName)
+);
+
+INSERT INTO Presets
+    (pName, rural_urban)
+VALUES
+    ('The World', 'Average'),
+    ('Asia', 'Average'),
+    ('Europe', 'Average');
 
 CREATE TABLE SubRegionPresets(
-    pName text,
-    subRegionCode char(3)
+    pName text NOT NULL,
+    subRegionCode char(3),
+    PRIMARY KEY (pName, subRegionCode),
+    FOREIGN KEY (pName) REFERENCES Presets
+        ON DELETE CASCADE
 );
-
 
 INSERT INTO SubRegionPresets
     (pName, subRegionCode)
@@ -56,56 +73,16 @@ VALUES
     ('The World', '419'), 
     ('The World', '151'), 
     ('The World', '021'), 
-    ('The World', '061');
-
-INSERT INTO SubRegionPresets
-    (pName, subRegionCode)
-VALUES
+    ('The World', '061'),
     ('Asia', '030'),
     ('Asia', '145'),
     ('Asia', '034'),
     ('Asia', '035'),
-    ('Asia', '143');
-
-INSERT INTO SubRegionPresets
-    (pName, subRegionCode)
-VALUES
+    ('Asia', '143'),
     ('Europe', '155'),
     ('Europe', '154'),
     ('Europe', '039'),
     ('Europe', '151');
-
-
-/* CREATE TABLE Waste(
-    iso integer,
-    iso_alpha text, 
-    year integer, 
-    commodity text,
-    loss_pct float, 
-    stage text          );              Could perhaps be changed to a integer (as index for a chronological stage number) */
-
-/*
-CREATE TABLE Waste(
-    m49_code text,
-    country text,
-    region text,
-    cpc_code text,
-    commodity text,
-    year text,
-    loss_percentage text,
-    loss_percentage_original text,
-    loss_quantity text,
-    activity text,
-    food_supply_stage text,
-    treatment text,
-    cause_of_loss text,
-    sample_size text,
-    method_data_collection text,
-    reference text,
-    url text,
-    notes text
-);
-*/
 
 \copy Countries FROM 'data_cleaned/countries_cleaned.csv' WITH CSV HEADER DELIMITER ',';
 \copy Affordability FROM 'data_cleaned/unaffordability_cleaned.csv' WITH CSV HEADER DELIMITER ',';
